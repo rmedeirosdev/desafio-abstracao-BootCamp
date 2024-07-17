eec66c22f1baac970140ce346faa4f9f28bcb05e
Priv <raphaelmedeiros2004@gmail.com>
2024-07-17 13:11:28 -0300

    Create Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso Java Descricao");
+        curso1.getCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Curso JS Descricao");
+        curso2.getCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Mentoria Java Descricao");
+        mentoria1.setData(LocalDate.now());
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        System.out.println(mentoria1);
     }
 }
---
+++ src/br/com/dio/desafio/dominio/BootCamp.java
@@ -0,0 +1,7 @@
+package br.com.dio.desafio.dominio;
+
+public class BootCamp {
+    public static void main(String[] args) {
+
+    }
+}
